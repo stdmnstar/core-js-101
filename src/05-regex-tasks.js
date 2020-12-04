e6484b12a22dd4c6aca84cc4ad6bb72b24e93e98
Aliaksandr Tarhonski <stdmnstar@gmail.com>
2020-12-04 11:57:23 +0300

    feat: solution regex task

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return /\{\p{Hex_Digit}{8}-\p{Hex_Digit}{4}-\p{Hex_Digit}{4}-\p{Hex_Digit}{4}-\p{Hex_Digit}{12}\}/u;
 }
 
 
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /p.t/;
 }
 
 
@@ -78,8 +78,8 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  return new RegExp(`(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)^[a-zA-Z\\d]{${minLength},}$`);
 }
 
 
